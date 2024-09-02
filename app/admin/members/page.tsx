be5a6a39b98b4ee0594f70e330d75a77a849c56b
AureaStar <aurea.starlight@gmail.com>
2024-09-02 01:18:29 -0300

    a

--- app/admin/members/page.tsx
+++ app/admin/members/page.tsx
@@ -4,10 +4,21 @@ import Title from "@/components/title";
 import { fetchMembers } from "@/back/adm/actions";
 import Link from "next/link";
 import Table from "@/components/admin/members/table";
+import { Membro } from "@prisma/client";
+
 
 export default async function Members() {
 
-    const {members, totalPages} = await fetchMembers();
+    let members: Membro[] = [];
+    let totalPages: number = 0;
+
+    try {
+        const response = await fetchMembers();
+        members = response.members;
+        totalPages = response.totalPages;
+    } catch (error) {
+        console.error('Erro ao buscar membros:', error);
+    }
 
   return (
     <main className="bg-re flex flex-col py-12 gap-12 items-end">
