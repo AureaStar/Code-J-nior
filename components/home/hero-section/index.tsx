93551c125be6342692b48fc3884ed7e079696e50
AureaStar <aurea.starlight@gmail.com>
2024-09-02 09:57:20 -0300

    landing-page

--- components/home/hero-section/index.tsx
+++ components/home/hero-section/index.tsx
@@ -1,16 +1,29 @@
 'use client';
 
+import Link from "next/link";
+
 export default function HeroSection() {
     return (
-        <section className="sm:h-screen md:h-screen lg:h-screen xl:h-screen test flex flex-col items-center justify-center bg-grar py-12 md:py-0 gap-4 w-full px-8 md:px-0">
-            <h1 className="text-xl md:text-4xl text-white font-intro text-center">Transformando ideias em softwares há 10 anos</h1>
-            <button className="bg-a text-re font-mont font-medium px-1 md:px-4 py-2 mt-4 rounded-md">Entre em contato agora mesmo</button>
-            <div className="flex flex-row justify-center items-center gap-2 md:gap-4 w-full">
-                <p className="text-white font-mont text-md md:text-2xl w-40 text-center">Conheça nossos <a href="#" className="text-a">projetos</a></p>
+        <section className="md:h-screen flex flex-col items-center justify-center bg-grar py-12 md:py-0 gap-4 w-full px-8 md:px-0 md:gap-8 lg:gap-24">
+            <h1 className="text-2xl md:text-4xl lg:text-5xl text-white font-intro text-center">Transformando ideias em softwares há 10 anos</h1>
+            <Link href="/contact">
+                <button className="bg-a text-re font-mont text-sm font-medium px-2 md:px-4 py-2 mt-4 rounded-md md:text-lg lg:text-xl lg:px-8 lg:py-4">Entre em contato agora mesmo</button>
+            </Link>
+            <div className="flex flex-row justify-center items-center gap-2 md:gap-4 lg:gap-8 w-full">
+                <div className="text-w flex flex-col justify-center items-center w-24">
+                    <span className="font-bold text-xl lg:text-4xl">14</span>
+                    <p className="text-white font-mont text-sm md:text-xl lg:text-2xl text-center"><a href="/portfolio" className="text-a">projetos</a> concluídos!</p>
+                </div>
                 <div className="border-r-2 bg-w border-white h-12 md:h-full mx-2"></div>
-                <p className="text-white font-mont text-md md:text-2xl w-40 text-center">ou <a href="#" className="text-a">serviços</a></p>
+                <div className="text-w flex flex-col justify-center items-center w-24">
+                    <span className="font-bold text-xl lg:text-4xl">10</span>
+                    <p className="text-white font-mont text-sm md:text-xl lg:text-2xl text-center"><a href="/about" className="text-a">anos</a> de mercado!</p>
+                </div>
                 <div className="border-r-2 bg-w border-white h-12 md:h-full mx-2"></div>
-                <p className="text-white font-mont text-md md:text-2xl w-40 text-center">ou <a href="#" className="text-a">sobre nós</a></p>
+                <div className="text-w flex flex-col justify-center items-center w-24">
+                    <span className="font-bold text-xl lg:text-4xl">67</span>
+                    <p className="text-white font-mont text-sm md:text-xl lg:text-2xl text-center"><a href="/admin/members" className="text-a">proficionais</a> formados!</p>
+                </div>
             </div>
         </section>
         )
--- components/home/services/index.tsx
+++ components/home/services/index.tsx
@@ -4,9 +4,9 @@ import Card from "./card";
 
 export default function Services() {
     return (
-        <section className="flex flex-col items-center justify-center px-8 py-4 bg-w gap-4 md:gap-16 md:h-[32rem] md:my-8">
+        <section className="flex flex-col items-center justify-center px-8 py-4 bg-w gap-4 md:gap-16  md:my-8">
             <h1 className="text-2xl md:text-4xl text-re font-intro">Serviços</h1>
-            <div className="flex flex-col md:flex-row gap-4 md:h-full justify-center md:gap-8">
+            <div className="flex flex-col lg:flex-row gap-4 md:h-full justify-center lg:gap-8">
                 <Card title="Sites Expositivos Personalizados" />
                 <Card title="Aplicativos" />
                 <Card title="Sistemas de Gerênciamento" />
